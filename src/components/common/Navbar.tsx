688a7c132df4fb4f0785670f2133408559711517
Shandrew <andresroman45678@gmail.com>
2024-08-16 20:44:24 -0500

    Update navbar link text from "Proyects" to "Projects"

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -69,7 +69,7 @@ export const Navbar = () => {
                       href="#proyects"
                       className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 transition ease-in-out delay-100 hover:transform hover:scale-150 hover:text-white"
                     >
-                      Proyects
+                      Projects
                     </a>
                     <a
                       href="#experience"
